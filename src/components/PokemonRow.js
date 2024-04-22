2db838d1e7788395228c22e1ccf86b67ddff40b9
Nikolai <niklonberg@gmail.com>
2024-04-22 13:07:38 +0200

    add button, onClick event to run onSelect

--- src/components/PokemonRow.js
+++ src/components/PokemonRow.js
@@ -4,6 +4,9 @@ export const PokemonRow = ({ pokemon, onSelect }) => (
   <tr>
     <td>{pokemon.name.english}</td>
     <td>{pokemon.type.join(", ")}</td>
+    <td>
+      <button onClick={() => onSelect(pokemon)}>Select</button>
+    </td>
   </tr>
 );
 
