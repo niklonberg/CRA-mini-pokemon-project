5636dc81956563ea7a541113bafb2246a4568e9b
Nikolai <niklonberg@gmail.com>
2024-04-22 12:09:35 +0200

    add proptypes to PokemonRow

--- src/components/PokemonRow.js
+++ src/components/PokemonRow.js
@@ -1,6 +1,15 @@
+import PropTypes from "prop-types";
+
 export const PokemonRow = ({ pokemon }) => (
   <tr>
     <td>{pokemon.name.english}</td>
     <td>{pokemon.type.join(", ")}</td>
   </tr>
 );
+
+PokemonRow.propTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.shape({ english: PropTypes.string }),
+    type: PropTypes.arrayOf(PropTypes.string),
+  }),
+};
