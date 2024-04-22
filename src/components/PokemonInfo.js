31c9cb71dbecabaa4970a2bd258b272f5e079845
Nikolai <niklonberg@gmail.com>
2024-04-22 13:27:34 +0200

    add prop types for PokemonInfo

--- src/components/PokemonInfo.js
+++ src/components/PokemonInfo.js
@@ -1,5 +1,20 @@
+import React from "react";
+import PropTypes from "prop-types";
+
 export const PokemonInfo = ({ name, base }) => (
   <div>
     <h1>{name.english}</h1>
   </div>
 );
+
+PokemonInfo.propTypes = {
+  name: PropTypes.shape({ english: PropTypes.string }),
+  base: PropTypes.shape({
+    HP: PropTypes.number.isRequired,
+    Attack: PropTypes.number.isRequired,
+    Defense: PropTypes.number.isRequired,
+    "Sp. Attack": PropTypes.number.isRequired,
+    "Sp. Defense": PropTypes.number.isRequired,
+    Speed: PropTypes.number.isRequired,
+  }),
+};
