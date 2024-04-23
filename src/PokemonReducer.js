5a029825eff216a4bd4765d069edf2c6fbeb0089
Nikolai <niklonberg@gmail.com>
2024-04-23 12:03:30 +0200

    add PokemonReducer

---
+++ src/PokemonReducer.js
@@ -0,0 +1,23 @@
+const PokemonReducer = (state, action) => {
+  switch (action) {
+    case "SET_FILTER":
+      return {
+        ...state,
+        filter: action.payload,
+      };
+    case "SET_POKEMON":
+      return {
+        ...state,
+        pokemon: action.payload,
+      };
+    case "SET_SELECTED_POKEMON":
+      return {
+        ...state,
+        selectedPokemon: action.payload,
+      };
+    default:
+      throw new Error("No action");
+  }
+};
+
+export default PokemonReducer
